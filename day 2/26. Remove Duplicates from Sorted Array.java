0cdd568ffe033e859c22ed58eacb9de23198ebcf
DeepakSaran3103 <100698417+DeepakSaran3103@users.noreply.github.com>
2022-12-20 15:45:11 +0530

    Create 26. Remove Duplicates from Sorted Array.java

---
+++ day 2/26. Remove Duplicates from Sorted Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int x=0;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i-1]==nums[i])
+            {
+                x++;
+            }
+            nums[i-x]=nums[i];
+        }
+        return nums.length-x;
+    }
+}
